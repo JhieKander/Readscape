c86c07fbb3e3ed7a66c02e37e3944929b25a396e
JhieKander <137230055+JhieKander@users.noreply.github.com>
2024-05-21 13:47:40 +0800

    index.php

---
+++ admin/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <?php require_once("../assets/parts/admin-head.php"); ?>
+<body>
+<?php require_once("../assets/inc/admin-nav.php"); ?>
+<hr>
+<?php require_once("../function/admin-session.php"); ?>
+
+<?php require_once("../assets/inc/admin-sidebar.php"); ?>
+<div class="col py-3">
+            
+        </div>
+    </div>
+</div>
+
+
+
+
+  <?php require_once("../assets/inc/footer.php"); ?>
+
+ <?php require_once("../assets/parts/admin-bottom.php"); ?>
