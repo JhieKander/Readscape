f78a258269eb79fe92cf739c42907704686c480b
JhieKander <137230055+JhieKander@users.noreply.github.com>
2024-05-21 13:46:06 +0800

    Add files via upload

---
+++ assets/parts/user-bottom.php
@@ -0,0 +1,12 @@
+
+<script type="text/javascript" src="assets/js/bootstrap.bundle.min.js"></script>
+<script type="text/javascript" src="assets/js/jQuery-3.6.0.min.js"></script>
+<script type="text/javascript" src="assets/js/all.min.js"></script>
+<script type="text/javascript" src="assets/js/aos.js"></script>
+<script type="text/javascript" src="assets/js/aos-init.js"></script>
+
+<script type="text/javascript" src="assets/js/product_carousel.js"></script>
+
+
+</body>
+</html>	
