f4b033e3a93cdac553feb17ae8b7a1641baa1119
JhieKander <137230055+JhieKander@users.noreply.github.com>
2024-05-21 13:41:55 +0800

    admin-footer.php

---
+++ assets/inc/admin-footer.php
@@ -0,0 +1,38 @@
+<footer class="footer mt-5" id="user-footer">
+    <hr>
+    <div class="container-fluid">
+        <div class="row">
+        <div class="col-md-3">
+             <h1 class="fw-bold">SHOP LOGO</h1>
+        </div>
+
+        <div class="col-md-3">
+             <p>For inquiries/concerns get in touch with us at</p>
+             <span><i class="fa fa-comments" aria-hidden="true"></i> Chat Support</span>
+        </div>
+        <div class="col-md-3">
+             <h6 class="fw-bold">Company & Services</h6>
+             <ul class="navbar-nav">
+                <li><a href="#" class="nav-link text-dark">About us</a></li>
+                <li><a href="#" class="nav-link text-dark">FAQs</a></li>
+                <li><a href="#" class="nav-link text-dark">Privacy and Cookie Policy</a></li>
+             </ul>
+        </div>
+
+        <div class="col-md-3">
+             <h6 class="fw-bold">FOLLOW OUR SOCIAL MEDIA</h6>
+             <div class="d-flex">
+               <a href="#"><img src="../assets/images/icon/facebook.png" class="img-fluid me-5" alt="" width="30"></a>
+               <a href="#"><img src="../assets/images/icon/instagram.png" class="img-fluid me-5" alt="" width="30"></a>             
+               <a href="#"><img src="../assets/images/icon/twitter.png" class="img-fluid" alt="" width="30"></a>             
+
+            </div>
+        </div>
+    
+    
+    </div>
+        
+
+
+    </div>
+</footer>
